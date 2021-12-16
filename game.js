731065271a5df26eb222e0b44e67da5d66cd228a
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-12-16 12:49:18 +0000

    :sparkles: Add all status codes, show answer states, fix some bugs

--- game.js
+++ game.js
@@ -4,35 +4,56 @@ import { getRandomInt, getRandomEntry } from "./utils.js";
 const quizHolder = document.querySelector("[data-quiz]");
 const startButton = document.querySelector("[data-start]");
 const resetButton = document.querySelector("[data-reset]");
+const submitButton = document.querySelector("[data-submit]");
 const questionEl = document.querySelector("[data-question]");
+const scoreEl = document.querySelector("[data-score]");
 const answerEls = document.querySelectorAll("[data-answer]");
 
 const GameState = {
   current: {
     questionIndex: -1,
     wrongAnswers: [],
+    correctAnswerRandString: "",
+    score: 0,
   },
 };
 
-function getRandomWrongAnswer(correctAnswer) {
-  const randomData = getRandomEntry(StatusCodes);
-  const answer = Object.values(randomData)[0];
+function generateRandomString() {
+  return Math.random().toString().substr(2, 8);
+}
 
-  if (answer !== correctAnswer && !GameState.current.wrongAnswers.includes(answer)) {
-    GameState.current.wrongAnswers.push(answer);
-    return answer;
+function getRandomWrongAnswer(correctAnswerString) {
+  // TODO — fake status codes to trick people
+
+  const randomData = getRandomEntry(StatusCodes);
+  const answerString = Object.values(randomData)[0];
+
+  if (
+    answerString !== correctAnswerString &&
+    !GameState.current.wrongAnswers.includes(answerString)
+  ) {
+    GameState.current.wrongAnswers.push(answerString);
+    return answerString;
   } else {
-    getRandomWrongAnswer(correctAnswer);
+    return getRandomWrongAnswer(correctAnswerString);
+  }
+}
+
+function uncheckAllAnswers() {
+  for (let el of answerEls) {
+    el.querySelector("input").checked = false;
   }
 }
 
 function resetGame() {
-  console.log("RESET GAME");
   showStartButton();
   hideQuiz();
   hideResetButton();
+  uncheckAllAnswers();
   GameState.current.questionIndex = -1;
   GameState.current.wrongAnswers = [];
+  GameState.current.score = 0;
+  updateScore();
 }
 
 function showQuiz() {
@@ -59,17 +80,21 @@ function hideResetButton() {
   resetButton.style.display = "none";
 }
 
+function updateScore() {
+  scoreEl.innerText = GameState.current.score;
+}
+
 function startGame() {
-  console.log("START GAME");
   resetGame();
   hideStartButton();
   showResetButton();
-  generateQuestion(0);
+  GameState.current.questionIndex = 0;
+  generateQuestion();
   showQuiz();
 }
 
-function generateQuestion(questionIndex) {
-  console.log("GENERATE QUESTION");
+function generateQuestion() {
+  clearAnswerResults();
   // do a few things with game state
 
   // get random status code data
@@ -78,7 +103,7 @@ function generateQuestion(questionIndex) {
   // extract code from key
   const code = Object.keys(questionData);
   // extract description from value
-  const correctAnswer = Object.values(questionData);
+  const correctAnswer = Object.values(questionData)[0];
 
   // set question up
   questionEl.innerText = code;
@@ -87,24 +112,83 @@ function generateQuestion(questionIndex) {
   const randomElIndex = getRandomInt(0, answerEls.length - 1);
 
   // populate correct answer
-  answerEls[randomElIndex].innerText = `${correctAnswer} (CORRECT)`;
+  answerEls[randomElIndex].querySelector("label").textContent = `${correctAnswer}`;
+
+  // generate correct answer random string to store in game state
+  const correctAnswerIdentifier = generateRandomString();
+  GameState.current.correctAnswerRandString = correctAnswerIdentifier;
+  // store string in DOM on correct answer
+  answerEls[randomElIndex].dataset.answer = correctAnswerIdentifier;
+  answerEls[randomElIndex].querySelector("input").dataset.identifier = correctAnswerIdentifier;
 
   // populate rest of answer elements with random descriptions
   // make sure the random description is not correctAnswer
   for (let i = 0; i < answerEls.length; i++) {
-    // get a random description
     const wrongAnswer = getRandomWrongAnswer(correctAnswer);
 
     // populate wrong answer DOM elements
-
     if (i !== randomElIndex) {
-      answerEls[i].textContent = `${wrongAnswer} (WRONG)`;
+      answerEls[i].querySelector("label").textContent = `${wrongAnswer}`;
+
+      // generate wrong answer random string to confuse hackers who view source!!
+      const wrongAnswerIdentifier = generateRandomString();
+      // store string in DOM on answer
+      answerEls[i].dataset.answer = wrongAnswerIdentifier;
+      answerEls[i].querySelector("input").dataset.identifier = wrongAnswerIdentifier;
     }
   }
 }
 
+function clearAnswerResults() {
+  for (let el of answerEls) {
+    el.className = "answer";
+  }
+}
+
+function showAnswerResults() {
+  const correctAnswerEl = document.querySelector(
+    `[data-answer="${GameState.current.correctAnswerRandString}"]`,
+  );
+
+  correctAnswerEl.className = "answer__correct";
+
+  for (let el of answerEls) {
+    if (el.dataset.answer !== GameState.current.correctAnswerRandString) {
+      el.className = "answer__incorrect";
+    }
+  }
+}
+
+function submitAnswer() {
+  // get selected answer in DOM
+  const selectedInputEl = document.querySelector("input:checked");
+
+  if (!selectedInputEl) {
+    return;
+  }
+
+  const selectedAnswerString = selectedInputEl.dataset.identifier;
+
+  if (selectedInputEl && selectedAnswerString === GameState.current.correctAnswerRandString) {
+    // correct answer!
+
+    GameState.current.score += 1;
+    updateScore();
+  } else {
+    // wrong answer!
+  }
+
+  showAnswerResults();
+
+  // TODO show next question button
+}
+
 startButton.addEventListener("click", function () {
   startGame();
+});
+
+submitButton.addEventListener("click", function () {
+  submitAnswer();
 });
 
 resetButton.addEventListener("click", function () {
