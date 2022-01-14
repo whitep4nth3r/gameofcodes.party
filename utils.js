c8968b295dd9c922963ff3a76a056c5eeb6fa958
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2022-01-14 14:21:09 +0000

    Add more greetings and move submit button closer to answers

--- utils.js
+++ utils.js
@@ -6,5 +6,11 @@ export function getRandomEntry(array) {
   return array[getRandomInt(0, array.length - 1)];
 }
 
-export const goodGreetings = ["Great work!"];
-export const badGreetings = ["Better luck next time!"];
+export const goodGreetings = ["Great work!", "What a pro!", "Amazing!", "Yes!", "Woop!"];
+export const badGreetings = [
+  "Better luck next time!",
+  "Keep trying!",
+  "You'll get it next time!",
+  "The best way to learn is to make mistakes!",
+  "This isn't easy, stick with it!",
+];
